521e940cedb4537aaae32a74e2b335e68ddfa158
anshu052005 <p20anshu05@gmail.com>
2026-07-19 10:06:56 +0530

    Email Validator

---
+++ Email_Validator/email2.py
@@ -0,0 +1,16 @@
+# a-z
+# 0-9
+# . , _ -> 1 time 
+#  @ -> 1time
+
+# abcd@gmail.com
+
+import re
+email_condition = "^[a-z]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$"
+user_email = input("Enter your email : ")
+
+if(re.search(email_condition , user_email)):
+    print("Valid EMail")
+else:
+    print("Invalid Email")
+    
